9baac21fc4a30b175a833e697b8dc4609a59b019
Joseph Maley <jmaley@clemson.edu>
2014-02-09 21:59:02 -0500

    added homepage

---
+++ www/app/views/index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+	<head>
+		<title>MeTube</title>
+		<link href = "/css/main.css" rel = "stylesheet" type = "text/css">
+	</head>
+
+	<body>
+		<div id = "mt-container">
+			<div id = "mt-header" class>
+				<div id = "mt-header-logo">
+					<a href = "/"><img src = "/images/logo1.png"></a>
+				</div>
+				<div id = "mt-header-content">
+					<form>
+						<input id = "mt-header-content-searchbar" type = "text"> 
+						<button id = "mt-header-content-searchbutton" type = "submit">
+							<img src = "images/search.png">
+						</button>
+					</form>
+				</div>
+				<div id = "mt-header-signin">
+					<a class = "button" href = "/signin.php">
+						Sign In
+					</a>
+				</div>
+			</div>
+			<div id = "mt-sidebar">
+				<br>
+				<div class = "mt-sidebar-block">
+						<div class = "mt-sidebar-block-header"> BROWSE </div>
+						<div class = "mt-sidebar-block-body">
+							Music<br>
+							Sports<br>
+							Gaming<br>
+							Education<br>
+							Movies<br>
+							TV Shows<br>
+							News
+						</div>
+				</div>
+				<br>
+				<div class = "mt-sidebar-block">
+						<div class = "mt-sidebar-block-header"> USER </div>
+						<div class = "mt-sidebar-block-body">
+							Uploaded<br>
+							Downloaded<br>
+							Viewed<br>
+							Favorited
+						</div>
+				</div>
+				<br>
+				<div class = "mt-sidebar-block">
+						<div class = "mt-sidebar-block-header"> PLAYLISTS </div>
+						<div class = "mt-sidebar-block-body">
+							To do<br>
+							To do<br>
+							To do<br>
+						</div>
+				</div>
+			</div>
+			<div id = "mt-welcome">
+				
+			</div>
+		</div>
+	</body>
+</html>
