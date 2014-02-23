f8a8594229242ab44d960c5053322014ce5843c6
Thomas Cannon <tcannon@codemonkeydev.com>
2014-02-23 11:27:33 -0500

    Change default database from `database` to `metube`
    * Adds a personal touch to the application
    * Lets us know exactly what database to look for.

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -1,7 +1,8 @@
 class mysql 
 {
     $mysqlPassword = ""
- 
+    $databaseName  = "metube"
+
     package 
     { 
         "mysql-server":
@@ -30,15 +31,15 @@ class mysql
     exec 
     { 
         "create-default-db":
-            unless => "/usr/bin/mysql -uroot -p$mysqlPassword database",
-            command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'create database `database`;'",
+            unless => "/usr/bin/mysql -uroot -p$mysqlPassword $databaseName",
+            command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'create database `$databaseName`;'",
             require => [Service["mysql"], Exec["set-mysql-password"]]
     }
 
     exec 
     { 
         "grant-default-db":
-            command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'grant all on `database`.* to `root@localhost`;'",
+            command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'grant all on `$databaseName`.* to `root@localhost`;'",
             require => [Service["mysql"], Exec["create-default-db"]]
     }
 }
