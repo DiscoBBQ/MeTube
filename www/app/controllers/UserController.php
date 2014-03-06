510a38c8a2eb0d44b06dc12e399cc41a7dedf051
Thomas Cannon <tcannon@codemonkeydev.com>
2014-03-05 22:04:57 -0500

    Automatically login the user when they are created
    * simplifies the process for the user
      * Note that the "remember me" flag is triggered.

--- www/app/controllers/UserController.php
+++ www/app/controllers/UserController.php
@@ -5,12 +5,13 @@ class UserController extends BaseController {
 
 	public function create()
 	{
-    Xdebug_break();
     $user = new User();
     $user->username = Input::get('username');
     $user->password = Input::get('password');
     $user->passwordConfirmation = Input::get('password_confirm');
 		if($user->save()){
+      //login the newly created user automatically
+      Auth::attempt(array('username' => $user->username, 'password' => $user->password), true);
       return Redirect::to('/');
     } else{
       return Redirect::to('/register');
