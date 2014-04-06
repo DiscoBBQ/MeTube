e02052bb4ccdb0ba8b1fb225c7a1c21bccdbadac
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-06 12:13:51 -0400

    Added Keyword Model,responsible for getting/creating/deleting keywords
    * Abstracts a lot of the code away from the Model class.

---
+++ www/app/models/Keyword.php
@@ -0,0 +1,43 @@
+<?php
+
+class Keyword{
+  static public function createKeywordsForMediaFromPhrase($media_id, $phrase){
+    $keywords = array_unique(preg_split("/[\s,;]+/", $phrase));
+
+    foreach($keywords as $keyword) {
+      self::createKeywordForMediaIfDoesntExist($media_id, $keyword);
+    }
+  }
+
+  static public function deleteAllKeywordsForMedia($media_id){
+    DB::statement("DELETE FROM keywords WHERE mediaid = ?", array($media_id));
+  }
+
+  static public function getKeywordStringForMediaAndItsTitle($media_id, $title){
+    $results = DB::select("SELECT keyword FROM keywords WHERE mediaid = ?", array($media_id));
+
+    $keywords = array();
+    foreach($results as $result) {
+      array_push($keywords, $result->keyword);
+    }
+
+    $titlewords = array();
+    foreach(explode(' ', $title) as $titleword) {
+      array_push($titlewords, $titleword);
+    }
+
+    $keywords = array_diff($keywords, $titlewords);
+
+    return implode(' ', $keywords);
+  }
+
+  static protected function createKeywordForMediaIfDoesntExist($media_id, $keyword){
+    $count = DB::select("SELECT COUNT(*) AS count FROM keywords WHERE mediaid = ? AND keyword = ?", array($media_id, $keyword));
+
+    if($count[0]->count <= 0){
+      DB::statement("INSERT INTO keywords (mediaid, keyword) VALUES (?,?)", array($media_id, $keyword));
+    }
+  }
+}
+
+?>
