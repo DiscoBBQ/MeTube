d3e8091e1f5b9467333b2c336d4da6c2aebda69a
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-05 22:57:54 -0400

    Create Playlist model. Handles creation/updating/managing items
    * Has unit tests written as well. Will become the central thing used
      for managing playlists.

---
+++ www/app/models/Playlist.php
@@ -0,0 +1,183 @@
+<?php
+
+class Playlist{
+  public $user_id;
+  public $title;
+  public $description;
+  protected $id;
+  public $items = array();
+  public $errors = array();
+
+  public function save(){
+    if($this->validate() == false){
+      return false;
+    }
+
+    if($this->id == NULL){
+      //insert the record into the DB
+      DB::statement("INSERT INTO playlist (user_id, title, description) VALUES (?,?,?)", array($this->user_id, $this->title, $this->description));
+      //get the ID of the last inserted record
+      $this->id = intval(DB::getPdo()->lastInsertId('id'));
+    } else{
+      //update the existing record in the DB
+      DB::statement("UPDATE playlist SET user_id = ?, title = ?, description = ? WHERE id = ?", array($this->user_id, $this->title, $this->description, $this->id));
+    }
+
+    //now that the playlist has been created, save the items associated with it!
+    $this->saveItems();
+    return true;
+  }
+
+  protected function saveItems(){
+    //if the playlist already exists, then we need to delete all items associated with the playlist
+    //before we re-add them in the new order
+    if($this->id != NULL){
+      $this->destroyPlaylistItems();
+    }
+
+    //loop through every item in the items array, inserting them into the DB
+    for ($i=0; $i < count($this->items) ; $i++) {
+      $media_id = $this->items[$i];
+      DB::statement("INSERT INTO playlist_item (media_id, playlist_id, item_order) VALUES(?,?,?)", array($media_id, $this->id, $i + 1));
+    }
+  }
+
+  public function destroy(){
+    DB::statement("DELETE FROM playlist WHERE id = ?", array($this->id));
+    $this->destroyPlaylistItems();
+  }
+
+  protected function destroyPlaylistItems(){
+    DB::statement("DELETE FROM playlist_item WHERE playlist_id = ?", array($this->id));
+  }
+
+  public function getID(){
+    return $this->id;
+  }
+
+  public function getOwner(){
+    return $this->id;
+  }
+
+
+  static public function getByID($id){
+    $result = DB::select("SELECT * FROM playlist WHERE ID = ? LIMIT 1", array($id));
+    if(count($result) == 0){
+      return NULL;
+    }
+    return self::buildPlaylistFromResult($result[0]);
+  }
+
+  static public function getAllPlaylistsForUserID($user_id){
+    $results = DB::select("SELECT * FROM playlist WHERE user_id = ? ORDER BY id", array($user_id));
+
+    $playlists = array();
+
+    foreach ($results as $result) {
+      array_push($playlists, self::buildPlaylistFromResult($result));
+    }
+
+    return $playlists;
+  }
+
+  static protected function buildPlaylistFromResult($result){
+    $playlist = new self();
+    if($result == NULL){
+      return NULL;
+    }
+
+    $playlist->id               = intval($result->id);
+    $playlist->title            = $result->title;
+    $playlist->description      = $result->description;
+    $playlist->user_id          = intval($result->user_id);
+
+    $playlist_items_results = DB::select("SELECT media_id FROM playlist_item WHERE playlist_id = ? ORDER BY item_order", array($playlist->id));
+
+    //Now get all the items associated with this playlist
+    foreach ($playlist_items_results as $playlist_item_result) {
+      $playlist->addMediaToPlaylist($playlist_item_result->media_id);
+    }
+
+    return $playlist;
+  }
+
+  public function addMediaToPlaylist($media_id){
+    $media_id = intval($media_id);
+    foreach ($this->items as $playlist_items) {
+      //Does this item already exist in the playlist? If yes, then don't add it again.
+      if($playlist_items == $media_id){
+        return;
+      }
+    }
+
+    //doesn't exist, so add the item to the array
+    array_push($this->items, $media_id);
+  }
+
+  public function removeMediaFromPlaylist($media_id){
+    $media_id = intval($media_id);
+    $i = 0;
+    while($i < count($this->items)){
+      if($this->items[$i] == $media_id){
+        break;
+      }
+      $i++;
+    }
+
+    //if we reached the end of the items and didn't find this item, them don't try to remove it.
+    if($i == count($this->items)){
+      return;
+    }
+
+    array_splice($this->items, $i, 1);
+  }
+
+  public function movePlaylistItemUp($index){
+    $this->swapItems($index - 1, $index);
+  }
+
+  public function movePlaylistItemDown($index){
+    $this->swapItems($index, $index + 1);
+  }
+
+  protected function swapItems($index_a, $index_b){
+    if(($index_a < 0 || $index_b < 0) || ($index_a >= count($this->items) || $index_b >= count($this->items))){
+      return;
+    }
+
+    $temp = $this->items[$index_a];
+    $this->items[$index_a] = $this->items[$index_b];
+    $this->items[$index_b] = $temp;
+  }
+
+  protected function sanitizeData(){
+    $this->title = trim($this->title);
+    $this->description = trim($this->description);
+  }
+
+  public function validate(){
+    $this->sanitizeData();
+
+    if($this->title === ""){
+      $this->errors["title"] = "Title cannot be blank";
+    }
+
+    if($this->description === ""){
+      $this->errors["description"] = "Description cannot be blank";
+    }
+
+
+    if(User::getByID($this->user_id) == NULL){
+      $this->errors["user_id"] = "User does not exist";
+    }
+
+    if(count($this->errors) > 0){
+      return false;
+    } else{
+      return true;
+    }
+  }
+
+}
+
+?>
---
+++ www/app/tests/unit/PlaylistTest.php
@@ -0,0 +1,349 @@
+<?php
+
+//Unit Tests for the user model
+class PlaylistUnitTest extends TestCase {
+  public $user_1;
+  public $user_2;
+  public $valid_playlist;
+
+  public function setUp(){
+    parent::setUp();
+    $this->user_1 = new User();
+    $this->user_1->channel_name = "text";
+    $this->user_1->email = "text@test.com";
+    $this->user_1->password = "test1234";
+    $this->user_1->passwordConfirmation = "test1234";
+    $this->user_1->save();
+
+    $this->user_2 = new User();
+    $this->user_2->channel_name = "next";
+    $this->user_2->email = "next@test.com";
+    $this->user_2->password = "test1234";
+    $this->user_2->passwordConfirmation = "test1234";
+    $this->user_2->save();
+
+    $this->valid_playlist = new Playlist();
+    $this->valid_playlist->title = "Test";
+    $this->valid_playlist->description = "hello World";
+    $this->valid_playlist->user_id = $this->user_1->getID();
+  }
+
+  public function testCreatePlaylist(){
+    $this->valid_playlist->validate();
+
+    $this->assertTrue($this->valid_playlist->save());
+
+    $this->assertNotNull($this->valid_playlist->getID());
+
+    $this->assertNotNull(Playlist::getByID($this->valid_playlist->getID()));
+  }
+
+  public function testTitleMustExist(){
+    $this->valid_playlist->title = "";
+    $this->assertFalse($this->valid_playlist->save());
+    $this->assertEquals($this->valid_playlist->errors["title"], "Title cannot be blank");
+  }
+
+  public function testPlaylistMustExist(){
+    $this->valid_playlist->description = "";
+    $this->assertFalse($this->valid_playlist->save());
+    $this->assertEquals($this->valid_playlist->errors["description"], "Description cannot be blank");
+  }
+
+  public function testUserMustExist(){
+    $this->valid_playlist->user_id = null;
+    $this->assertFalse($this->valid_playlist->save());
+    $this->assertEquals($this->valid_playlist->errors["user_id"], "User does not exist");
+  }
+
+  public function testUpdatingPlaylist(){
+    $this->valid_playlist->save();
+    $old_id = $this->valid_playlist->getID();
+
+    $this->valid_playlist->title = "Hello world";
+    $this->assertTrue($this->valid_playlist->save());
+    $this->assertEquals($this->valid_playlist->getID(), $old_id);
+
+    $this->assertNotNull($this->valid_playlist->getID());
+    $new_message = Playlist::getByID($this->valid_playlist->getID());
+    $this->assertEquals($new_message->title, "Hello world");
+  }
+
+  public function testOnlyUpdatesOnePlaylist(){
+    $this->valid_playlist->save();
+    $old_id = $this->valid_playlist->getID();
+
+    $other_playlist = new Playlist();
+    $other_playlist->title = "other message";
+    $other_playlist->description = "Stuff to do";
+    $other_playlist->user_id = $this->user_1->getID();
+    $other_playlist->save();
+
+    $other_playlist_id = $other_playlist->getID();
+
+
+    $this->valid_playlist->title = "test";
+    $this->assertTrue($this->valid_playlist->save());
+    $this->assertEquals($this->valid_playlist->getID(), $old_id);
+    $this->assertNotNull($this->valid_playlist->getID());
+
+    $new_message = Playlist::getByID($other_playlist_id);
+    $this->assertEquals($new_message->title, "other message");
+  }
+
+  public function testfindAllPlaylistsForUser(){
+    $this->valid_playlist->save();
+    $result = Playlist::getAllPlaylistsForUserID($this->user_1->getID());
+    $this->assertEquals(count($result), 1);
+    $this->assertEquals($result[0]->getID(), $this->valid_playlist->getID());
+  }
+
+  public function testreturnEmptyArrayIfNoPlaylistsSentForUser(){
+    $this->valid_playlist->save();
+    $result = array();
+    $this->assertEquals(Playlist::getAllPlaylistsForUserID(1234), $result);
+  }
+
+  public function testonlyReturnPlaylistsForUser(){
+    $this->valid_playlist->save();
+
+    $other_playlist = new Playlist();
+    $other_playlist->title = "other";
+    $other_playlist->description = "not in list";
+    $other_playlist->user_id = $this->user_2->getID();
+    $other_playlist->save();
+
+    $result = array($this->valid_playlist->getID());
+    $message_ids = array();
+    foreach(Playlist::getAllPlaylistsForUserID($this->user_1->getID()) as $message){
+      array_push($message_ids, $message->getID());
+    }
+
+    $this->assertEquals($message_ids, $result);
+  }
+
+
+  public function testAddItemsToPlaylist(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),1);
+    $this->assertEquals($result->items[0],1);
+
+    $result->addMediaToPlaylist(3);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),2);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],3);
+  }
+
+  public function testRemoveItemsFromPlaylist(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),2);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+
+    $result->removeMediaFromPlaylist(5);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),1);
+    $this->assertEquals($result->items[0],1);
+  }
+
+  public function testRemoveItemsFromTopOfPlaylist(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->removeMediaFromPlaylist(1);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),2);
+    $this->assertEquals($result->items[0],5);
+    $this->assertEquals($result->items[1],2);
+  }
+
+  public function testRemoveItemsFromBottomOfPlaylist(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->removeMediaFromPlaylist(2);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),2);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+  }
+
+  public function testRemoveAllItemsFromPlaylist(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->removeMediaFromPlaylist(2);
+    $result->removeMediaFromPlaylist(1);
+    $result->removeMediaFromPlaylist(5);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),0);
+  }
+
+  public function movePlaylistItemUp(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->movePlaylistItemUp(1);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+
+    $this->assertEquals($result->items[0],5);
+    $this->assertEquals($result->items[1],1);
+    $this->assertEquals($result->items[2],2);
+  }
+
+  public function movePlaylistItemDown(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->movePlaylistItemUp(1);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],2);
+    $this->assertEquals($result->items[2],5);
+  }
+
+  public function moveTopPlaylistItemUp(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->movePlaylistItemUp(0);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+  }
+
+  public function moveBottomPlaylistItemDown(){
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+
+    $result->movePlaylistItemUp(2);
+
+    $result->save();
+    $result = Playlist::getByID($this->valid_playlist->getID());
+
+    $this->assertEquals(count($result->items),3);
+
+    $this->assertEquals($result->items[0],1);
+    $this->assertEquals($result->items[1],5);
+    $this->assertEquals($result->items[2],2);
+  }
+
+  public function testDestroy(){
+    $this->valid_playlist->save();
+    $this->valid_playlist->addMediaToPlaylist(1);
+    $this->valid_playlist->addMediaToPlaylist(5);
+    $this->valid_playlist->addMediaToPlaylist(2);
+    $this->valid_playlist->save();
+
+    $old_id = $this->valid_playlist->getID();
+
+    $this->valid_playlist->destroy();
+
+    $this->assertEquals(DB::selectOne("SELECT COUNT(*) as count FROM playlist_item WHERE playlist_id = ?", array($old_id))->count, 0);
+
+    $this->assertNull(Playlist::getByID($old_id));
+  }
+}
