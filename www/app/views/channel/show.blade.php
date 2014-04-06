fea373f5c9efad03beec2f98e25b9ea1514160b7
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-06 00:43:39 -0400

    Remove old Channel view, no longer used.

--- www/app/views/channel/show.blade.php
+++
@@ -1,9 +0,0 @@
-@section('content')
-  @foreach($results as $result)
-    <? $media = Media::getByID($result->id); ?>
-
-    @include('media.preview-block', array('media' => $media))
-  @endforeach
-
-  @include('partials.pagination', array('params' => $pagination_params, 'current_page' => $current_page, 'count' => $count, 'route_name' => $route_name))
-@stop
