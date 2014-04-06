ef03a421843bed8f13fa509625a7266d84ff4bf2
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-06 12:31:04 -0400

    Refactor Upload form to use Laravel input helpers
    * These helpers can rebuild from the previously sent values via
      `withInput()`

--- www/app/views/media/new.blade.php
+++ www/app/views/media/new.blade.php
@@ -8,8 +8,8 @@ The following errors prevented the message from being sent
 		<div class = "mt-form-box">
 			@include('partials.error-messages', array('error_messages' => $error_messages))
 			{{ Form::open(array('route' => 'create_media', 'files' => true)) }}
- 				<label for="title">Title:</label><input type = "text" name = "title"><br>
-				<label for="description">Description:</label><input type = "text" name = "description"><br>
+ 				<label for="title">Title:</label>{{Form::text('title')}}<br>
+				<label for="description">Description:</label>{{Form::text('description')}}<br>
 				<label for="category">Category:</label>
 					{{ Form::select('category', array('Music' => 'Music',
 							'Sports' => 'Sports',
@@ -19,10 +19,8 @@ The following errors prevented the message from being sent
 							'TV' => 'TV Shows'));
 					}}
 				<br>
-				<label for="keywords">Keywords:</label><input type = "text" name = "keywords">
-				<br>
-				<label for="file">File:</label><input type= "file" name = "file" id = "file">
-				<br>
+				<label for="keywords">Keywords:</label>{{ Form::text("keywords") }}<br>
+				<label for="file">File:</label>{{ Form::file('file')}}<br>
 				<input type = "submit" class = "mt-form-submit" value = "UPLOAD">
 			{{ Form::close() }}
 		</div>
