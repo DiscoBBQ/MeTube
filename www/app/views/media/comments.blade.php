f863a7fd6a8dd775ea16f6256d751f277dd6a813
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-06 13:44:15 -0400

    Add Comment "linking" to allow users to link to individual comments.

--- www/app/views/media/comments.blade.php
+++ www/app/views/media/comments.blade.php
@@ -1,8 +1,8 @@
 <?php $comments = Comment::getAllCommentsForMedia($media->getID()) ?>
 
 @foreach($comments as $comment)
-  <div class = "mt-sidebar-block">
-    <div class = "mt-block-title"><a href="{{route('profile', array('id' => $comment->getCommentor()->getID()))}}">{{$comment->getCommentor()->channel_name}}</a></div>
+  <div id="comment-{{$comment->getID()}}" class = "mt-sidebar-block">
+    <div class = "mt-block-title"><a href="#comment-{{$comment->getID()}}">#{{$comment->getID()}}</a> <a href="{{route('profile', array('id' => $comment->getCommentor()->getID()))}}">{{$comment->getCommentor()->channel_name}}</a></div>
     <div class = "mt-sidebar-block-body">{{$comment->comment}}</div>
   </div>
 @endforeach
