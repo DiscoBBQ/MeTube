c263d915e9152fabe272f0b01a52ef5e55a302cb
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-19 18:41:54 -0400

    Refactor Browsing Styling.

--- www/app/views/media/preview-block.blade.php
+++ www/app/views/media/preview-block.blade.php
@@ -1,19 +1,30 @@
-<div id = "welcome-browse-block-indv">
-  <div class = "block-title-left">
+<div class= "preview-block">
+  <div class = "block-title block-title-left preview-block-title">
     <a href="{{route('media', array('id' => $media->getID()))}}">{{$media->title}}</a>
   </div>
-  <div id='welcome-browse-block-body'>
-    <a href="{{route('media', array('id' => $media->getID()))}}">{{$media->title}}
-      <div class = "browse-img-border">
-        <img class = "browse-img" src="{{asset($media->getThumbnail())}}">
+  <div class='preview-block-body'>
+    <div class="preview-thumbnail">
+      <a href="{{route('media', array('id' => $media->getID()))}}">
+        @include('media.thumbnail', array("media" => $media))
+      </a>
+      <div class="media-stats">
+        <ul>
+          <li>{{ Interaction::countViewedForMedia($media->getID()) }} <span class="oi views-icon" data-glyph="eye"></span></li>
+          <li>{{ Interaction::countDownloadedForMedia($media->getID()) }} <span class="oi downloads-icon" data-glyph="data-transfer-download"></span></li>
+          <li>{{ Interaction::countFavoritedForMedia($media->getID()) }} <span class="oi favorites-icon" data-glyph="star"></span></li>
+        <ul>
       </div>
-    </a>
+    </div>
 
-    <div id = "welcome-browse-block-info">
-      Author: <a class = "text-link" href = "{{ route('profile', array('id' => $media->getAuthor()->getID())) }}">{{$media->getAuthor()->channel_name}}</a><br>
-      Category: <a class = "text-link" href = "{{ route('browse_category', array('category' => $media->category)) }}">{{$media->category}}</a><br>
-      Description: {{ $media->description }}
-
+    <div class = "preview-block-info">
+      <div class="author">
+        <strong>Author:</strong> <a class = "text-link" href = "{{ route('profile', array('id' => $media->getAuthor()->getID())) }}">{{$media->getAuthor()->channel_name}}</a>
+      </div>
+      
+      <div class="category">
+        <strong>Category:</strong> <a class = "text-link" href = "{{ route('browse_category', array('category' => $media->category)) }}">{{$media->category}}</a>
+      </div>
+      <div class="description">{{ $media->description }}</div>
       @yield('playlist-ordering','')
     </div>
   </div>
