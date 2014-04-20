db3bb913f17b273e4202171f5e08619c390e4787
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-20 00:53:15 -0400

    Restyle "Edit Profile" Form

--- www/app/views/users/edit.blade.php
+++ www/app/views/users/edit.blade.php
@@ -7,23 +7,23 @@ The following errors prevented the profile from being updated
 @stop
 
 @section('content')
-  <div id = "mt-upload-inner">
-    <div class = "block-title">Upload</div>
-      <div class = "mt-form-box">
-        @include('partials.error-messages', array('error_messages' => $error_messages))
-        {{Form::open(array('route' => 'update_profile'))}}
-          E-mail: {{Form::email('email', $user->email)}}<br>
-          Channel Name: {{Form::text('channel_name', $user->channel_name)}}<br>
-          <div id="password-change">
-            Current Password: <input type = "password" name = "current_password"><br>
-            New Password: <input type = "password" name = "password"><br>
-            Confirm New Password: <input type = "password" name = "password_confirm"><br>
-          </div>
-          <div class = "form-button-body">
-            <input type = "submit" class = "form-button" value = "Update Profile">
-          </div>
-        {{Form::close()}}
+  {{Form::open(array('route' => 'update_profile', 'class' => 'entry_form', 'id' => 'edit_prpfile'))}}
+    <h2 class="block-title">Edit Profile</h2>
+    @include('partials.error-messages', array('error_messages' => $error_messages))
+    <div class="form_content">
+      <label for="email">Email:</label> {{Form::email('email', $user->email)}}<br>
+      <label for="channel_name">Channel Name:</label> {{Form::text('channel_name', $user->channel_name)}}<br>
+      <div id="password-change">
+        <label for="current_password">Current Password:</label><input type = "password" name = "current_password"><br>
+        <label for="password">New Password:</label><input type = "password" name = "password"><br>
+        <label for="password_confirm">Confirm New Password:</label><input type = "password" name = "password_confirm"><br>
       </div>
+      <div id = "form_actions">
+        <button type="submit" class="form-button"><span class="oi" data-glyph="circle-check"></span> Update Profile</button>
+        <button type="reset" class="form-button"><span class="oi" data-glyph="circle-x"></span> Reset Form</button>
+      </div>
+    </div>
+  {{Form::close()}}
     </div>
   </div>
 @stop
