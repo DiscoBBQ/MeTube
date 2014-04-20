0f921b13e4227b909e329315192bbe382aca323e
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-20 01:03:08 -0400

    Restyle New and Edit Playlist Forms.

--- www/app/views/playlists/edit.blade.php
+++ www/app/views/playlists/edit.blade.php
@@ -7,18 +7,17 @@ The following errors prevented the playlist from being updated
 @stop
 
 @section('content')
-  <div id = "mt-upload-inner">
-    <div class = "block-title">
-      Edit Playlist
+  {{ Form::open(array('route' => array('update_playlist', $playlist->getID()), 'class' => 'entry_form', 'id' => 'edit_playlist')) }}
+    <h2 class="block-title">Edit Playlist</h2>
+    @include('partials.error-messages', array('error_messages' => $error_messages))
+    <div class="form_content">
+      <label for="title">Title:</label><br>{{ Form::text('title', $playlist->title) }}<br>
+      <label for="description">Description:</label><br/>
+      {{ Form::textarea('description', $playlist->description) }}<br>
+      <div id="form_actions">
+        <button type="submit" class="form-button"><span class="oi" data-glyph="circle-check"></span> Update Playlist</button>
+        <button type="reset" class="form-button"><span class="oi" data-glyph="circle-x"></span> Reset Form</button>
+      </div>
     </div>
-    <div class = "mt-form-box">
-      @include('partials.error-messages', array('error_messages' => $error_messages))
-      {{ Form::open(array('route' => array('update_playlist', $playlist->getID()))) }}
-        <label for="title">Title: </label> {{ Form::text('title', $playlist->title) }}<br>
-        <label for="description">Description: </label><br/>
-        {{ Form::textarea('description', $playlist->description) }}<br>
-        <input type = "submit" class = "form-button" value = "Update">
-      {{ Form::close() }}
-    <div>
-  </div>
+  {{ Form::close() }}
 @stop
--- www/app/views/playlists/new.blade.php
+++ www/app/views/playlists/new.blade.php
@@ -7,18 +7,17 @@ The following errors prevented the playlist from being created
 @stop
 
 @section('content')
-  <div id = "mt-upload-inner">
-    <div class = "block-title">
-      New Playlist
+  {{ Form::open(array('route' => 'create_playlist', 'class' => 'entry_form', 'id' => 'new_playlist')) }}
+    <h2 class="block-title">New Playlist</h2>
+    @include('partials.error-messages', array('error_messages' => $error_messages))
+    <div class="form_content">
+      <label for="title">Title:</label><br>{{Form::text('title')}}<br>
+      <label for="description">Description: </label><br/>
+      {{ Form::textarea('description') }}<br>
+      <div id="form_actions">
+        <button type="submit" class="form-button"><span class="oi" data-glyph="circle-check"></span> Create Playlist</button>
+        <button type="reset" class="form-button"><span class="oi" data-glyph="circle-x"></span> Reset Form</button>
+      </div>
     </div>
-    <div class = "mt-form-box">
-      @include('partials.error-messages', array('error_messages' => $error_messages))
-      {{ Form::open(array('route' => 'create_playlist')) }}
-        <label for="title">Title: </label>{{Form::text('title')}}<br>
-        <label for="description">Description: </label><br/>
-        {{ Form::textarea('description') }}<br>
-        <input type = "submit" class = "form-button" value = "CREATE">
-      {{ Form::close() }}
-    <div>
-  </div>
+  {{ Form::close() }}
 @stop
