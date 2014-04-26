f7d40f1def762b8a0fe163474dd063022cae0c96
Thomas Cannon <tcannon@codemonkeydev.com>
2014-04-25 21:21:52 -0400

    Updated database schema dump to build database if necessary.

--- database_schema.sql
+++ database_schema.sql
@@ -1,31 +1,35 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: mmlab.cs.clemson.edu (MySQL 5.5.29-0ubuntu0.12.04.1)
-# Database: u5
-# Generation Time: 2014-04-22 13:04:57 +0000
-# ************************************************************
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 25, 2014 at 09:17 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.3.10-1ubuntu3.11
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Database: `u5`
+--
+CREATE DATABASE IF NOT EXISTS `u5` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `u5`;
 
-# Dump of table comments
-# ------------------------------------------------------------
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
 
 DROP TABLE IF EXISTS `comments`;
-
-CREATE TABLE `comments` (
+CREATE TABLE IF NOT EXISTS `comments` (
   `user_id` int(11) unsigned DEFAULT NULL,
   `media_id` int(11) unsigned DEFAULT NULL,
   `comment` text,
@@ -33,51 +37,68 @@ CREATE TABLE `comments` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`),
   KEY `comments_user_fk` (`user_id`),
-  KEY `comments_media_fk` (`media_id`),
-  CONSTRAINT `comments_media_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `comments_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `comments_media_fk` (`media_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
+--
+-- RELATIONS FOR TABLE `comments`:
+--   `media_id`
+--       `media` -> `id`
+--   `user_id`
+--       `users` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table interactions
-# ------------------------------------------------------------
+--
+-- Table structure for table `interactions`
+--
 
 DROP TABLE IF EXISTS `interactions`;
-
-CREATE TABLE `interactions` (
+CREATE TABLE IF NOT EXISTS `interactions` (
   `user_id` int(11) unsigned DEFAULT NULL,
   `media_id` int(11) unsigned DEFAULT NULL,
   `category` varchar(32) NOT NULL DEFAULT '',
   `count` int(11) DEFAULT '1',
   UNIQUE KEY `interaction_set` (`user_id`,`media_id`,`category`),
-  KEY `interactions_media_fk` (`media_id`),
-  CONSTRAINT `interactions_media_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `interactions_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `interactions_media_fk` (`media_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONS FOR TABLE `interactions`:
+--   `media_id`
+--       `media` -> `id`
+--   `user_id`
+--       `users` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table keywords
-# ------------------------------------------------------------
+--
+-- Table structure for table `keywords`
+--
 
 DROP TABLE IF EXISTS `keywords`;
-
-CREATE TABLE `keywords` (
+CREATE TABLE IF NOT EXISTS `keywords` (
   `mediaid` int(11) unsigned NOT NULL,
   `keyword` varchar(255) NOT NULL,
-  PRIMARY KEY (`mediaid`,`keyword`),
-  CONSTRAINT `keywords_media_fk` FOREIGN KEY (`mediaid`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`mediaid`,`keyword`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONS FOR TABLE `keywords`:
+--   `mediaid`
+--       `media` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table media
-# ------------------------------------------------------------
+--
+-- Table structure for table `media`
+--
 
 DROP TABLE IF EXISTS `media`;
-
-CREATE TABLE `media` (
+CREATE TABLE IF NOT EXISTS `media` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `description` text,
@@ -86,18 +107,23 @@ CREATE TABLE `media` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `category` varchar(127) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `media_user_fk` (`authorid`),
-  CONSTRAINT `media_user_fk` FOREIGN KEY (`authorid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `media_user_fk` (`authorid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=29 ;
 
+--
+-- RELATIONS FOR TABLE `media`:
+--   `authorid`
+--       `users` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table messages
-# ------------------------------------------------------------
+--
+-- Table structure for table `messages`
+--
 
 DROP TABLE IF EXISTS `messages`;
-
-CREATE TABLE `messages` (
+CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `to_user_id` int(11) unsigned DEFAULT NULL,
   `from_user_id` int(11) unsigned DEFAULT NULL,
@@ -108,83 +134,107 @@ CREATE TABLE `messages` (
   PRIMARY KEY (`id`),
   KEY `messages_from_user_fk` (`from_user_id`),
   KEY `messages_parent_message_fk` (`parent_message_id`),
-  KEY `messages_to_user_fk` (`to_user_id`),
-  CONSTRAINT `messages_to_user_fk` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `messages_from_user_fk` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `messages_parent_message_fk` FOREIGN KEY (`parent_message_id`) REFERENCES `messages` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `messages_to_user_fk` (`to_user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
+--
+-- RELATIONS FOR TABLE `messages`:
+--   `from_user_id`
+--       `users` -> `id`
+--   `parent_message_id`
+--       `messages` -> `id`
+--   `to_user_id`
+--       `users` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table migrations
-# ------------------------------------------------------------
+--
+-- Table structure for table `migrations`
+--
 
 DROP TABLE IF EXISTS `migrations`;
-
-CREATE TABLE `migrations` (
+CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
-# Dump of table playlist
-# ------------------------------------------------------------
+--
+-- Table structure for table `playlist`
+--
 
 DROP TABLE IF EXISTS `playlist`;
-
-CREATE TABLE `playlist` (
+CREATE TABLE IF NOT EXISTS `playlist` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `description` text,
   PRIMARY KEY (`id`),
-  KEY `playlist_user_fk` (`user_id`),
-  CONSTRAINT `playlist_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `playlist_user_fk` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- RELATIONS FOR TABLE `playlist`:
+--   `user_id`
+--       `users` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table playlist_item
-# ------------------------------------------------------------
+--
+-- Table structure for table `playlist_item`
+--
 
 DROP TABLE IF EXISTS `playlist_item`;
-
-CREATE TABLE `playlist_item` (
+CREATE TABLE IF NOT EXISTS `playlist_item` (
   `item_order` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `media_id` int(11) unsigned NOT NULL DEFAULT '0',
   `playlist_id` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`item_order`,`media_id`,`playlist_id`),
   KEY `playlist_item_media_fk` (`media_id`),
-  KEY `playlist_item_playlist_fk` (`playlist_id`),
-  CONSTRAINT `playlist_item_playlist_fk` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `playlist_item_media_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `playlist_item_playlist_fk` (`playlist_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
+--
+-- RELATIONS FOR TABLE `playlist_item`:
+--   `media_id`
+--       `media` -> `id`
+--   `playlist_id`
+--       `playlist` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table subscriptions
-# ------------------------------------------------------------
+--
+-- Table structure for table `subscriptions`
+--
 
 DROP TABLE IF EXISTS `subscriptions`;
-
-CREATE TABLE `subscriptions` (
+CREATE TABLE IF NOT EXISTS `subscriptions` (
   `subscribing_user_id` int(11) unsigned NOT NULL DEFAULT '0',
   `subscription_user_id` int(11) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`subscribing_user_id`,`subscription_user_id`),
-  KEY `subscriptions_subscription_user_fk` (`subscription_user_id`),
-  CONSTRAINT `subscriptions_subscription_user_fk` FOREIGN KEY (`subscription_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `subscriptions_subscribing_user_fk` FOREIGN KEY (`subscribing_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `subscriptions_subscription_user_fk` (`subscription_user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONS FOR TABLE `subscriptions`:
+--   `subscribing_user_id`
+--       `users` -> `id`
+--   `subscription_user_id`
+--       `users` -> `id`
+--
 
+-- --------------------------------------------------------
 
-# Dump of table users
-# ------------------------------------------------------------
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `channel_name` varchar(50) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -194,14 +244,66 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`channel_name`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- Constraints for dumped tables
+--
 
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_media_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `interactions`
+--
+ALTER TABLE `interactions`
+  ADD CONSTRAINT `interactions_media_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `interactions_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+--
+-- Constraints for table `keywords`
+--
+ALTER TABLE `keywords`
+  ADD CONSTRAINT `keywords_media_fk` FOREIGN KEY (`mediaid`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_user_fk` FOREIGN KEY (`authorid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_from_user_fk` FOREIGN KEY (`from_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_parent_message_fk` FOREIGN KEY (`parent_message_id`) REFERENCES `messages` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `messages_to_user_fk` FOREIGN KEY (`to_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD CONSTRAINT `playlist_user_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `playlist_item`
+--
+ALTER TABLE `playlist_item`
+  ADD CONSTRAINT `playlist_item_media_fk` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `playlist_item_playlist_fk` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `subscriptions`
+--
+ALTER TABLE `subscriptions`
+  ADD CONSTRAINT `subscriptions_subscribing_user_fk` FOREIGN KEY (`subscribing_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `subscriptions_subscription_user_fk` FOREIGN KEY (`subscription_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
